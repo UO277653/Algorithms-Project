1b2d8dd541582e8c352fcd4a2dc1d299a5304802
adria <adria@192.168.56.1>
2021-03-02 22:07:43 +0100

    Updated session 2 files and added the pdf of the practice

---
+++ src/main/java/algstudent/s2/Insertion.java
@@ -0,0 +1,34 @@
+﻿package algstudent.s2;
+
+
+/* This program can be used to sort n elements with 
+ * a "bad" algorithm (quadratic). 
+ * It is the DIRECT INSERTION */
+public class Insertion extends Vector {	
+	public Insertion(int nElements) {
+		super(nElements);
+	}
+
+	@Override
+	public void sort(){
+		int counter;
+		int pivot;
+		
+		for(int i = 1; i < elements.length; i++) {
+			pivot = elements[i];
+			counter = i-1;
+			
+			while(counter>=0 && pivot<elements[counter]) {
+				elements[counter+1] = elements[counter];
+				counter--;
+			}
+			
+			elements[counter+1] = pivot;
+		}
+	}
+
+	@Override
+	public String getName() {
+		return "Insertion";
+	} 
+} 
---
+++ src/main/java/algstudent/s2/Selection.java
@@ -0,0 +1,39 @@
+﻿package algstudent.s2;
+
+/* This program can be used to sort n elements with 
+ * a "bad" algorithm (quadratic). 
+ * It is the SELECTION */
+public class Selection extends Vector {
+	public Selection(int nElements) {
+		super(nElements);
+	}
+	
+	@Override
+	public void sort() {
+		int min;
+		
+		for(int i = 0; i < elements.length-1; i++) {
+			min = getMinimumPosition(i);
+			interchange(i, min);
+		}
+	}  
+	
+	private int getMinimumPosition(int val) {
+		int minimum = Integer.MAX_VALUE;
+		int res = 0;
+		
+		for(int i = val; i < elements.length; i++) {
+			if(elements[i] < minimum) {
+				minimum = elements[i];
+				res = i;
+			}
+		}
+		
+		return res;
+	}
+	
+	@Override
+	public String getName() {
+		return "Selection";
+	} 
+} 
